a8d73d05a623cddc7bf7d9c9e7872db0d1d257e7
quki <gabrielbrufau@gmail>
2021-12-11 11:03:56 -0300

    45

--- GO/bucles/for.go
+++ GO/bucles/for.go
@@ -3,13 +3,13 @@ package main
 import "fmt"
 
 func main(){
-	//this is a for normal
+//this is a for normal
 	i := 0;
 	for i<10 {
 		fmt.Println(i)
 		i++
 	}
-	//break an cicle infinite
+//break an cicle infinite
 	j :=0;
 	for {
 		if i>9{
@@ -19,7 +19,7 @@ func main(){
 		i++
 	}
 
-	// modulo % da el resto de una divicion
+// modulo % da el resto de una divicion
 	x:=1
 
 	for {
@@ -33,7 +33,7 @@ func main(){
 		fmt.Println(x)
 	}
 
-	//how print values acii whit for
+//how print values acii whit for
 	for i:=33; i<=122; i++{
 		fmt.Printf("%d\t%#x\t%#U\n",i,i,i)
 }
--- GO/bucles/for.md
+++
@@ -1,7 +0,0 @@
-# for
-```go
-for i := 0; i<len(s1);i++{
-  fmt.Printf("%#U",s1[i])
-}
-
-```
