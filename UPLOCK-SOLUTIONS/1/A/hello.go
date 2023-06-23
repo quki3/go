5857a6d3d02d9b7e80a56d89768f51e8513580ff
Gabriel Nestor Brufau <70489784+qukiiignb@users.noreply.github.com>
2023-06-23 06:46:10 -0300

    Update hello.go

--- UPLOCK-SOLUTIONS/1/A/hello.go
+++ UPLOCK-SOLUTIONS/1/A/hello.go
@@ -1,22 +1,15 @@
-/*package main
-	it group functions and it's made up of all the files in the same directory
-*/
+// package main - it group functions and it's made up of all the files in the same directory is a package for executions
 package main
 
-/*
-fmt
-contains functions for formatting text, including: Println
-*/
+// import - get some external package for use
+// fmt - standard library, contains functions for formatting text, including: Println
 import "fmt"
 
-/*
-func main
-is the enter poin of go
-*/
-func main() {
-
-		fmt.Println("Hello, World!")
-
+// func - define a function
+// main - is the enter poin of go
+func main(){
+        // Println - print formated oupput usually in the console or Terminal
+	fmt.Println("Hello, World!")
 }
 
 
