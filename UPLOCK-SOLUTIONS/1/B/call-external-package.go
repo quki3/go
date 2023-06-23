f63b343c2915088059d069a54bd9c8525ee676f9
Gabriel Nestor Brufau <70489784+qukiiignb@users.noreply.github.com>
2023-06-23 07:51:11 -0300

    Update callExternalPackage.go

--- UPLOCK-SOLUTIONS/1/B/call-external-package.go
+++ UPLOCK-SOLUTIONS/1/B/call-external-package.go
@@ -1,21 +1,20 @@
-/* package main
-group functions */
+// package - collection of software file.
+// main -special package named in go this must have a main() witch serves as a entri poin of the programs and It is used for creating standalone ejecutable programs
 package main
 
-/* import
-when you need your code to do something that might have been implemented by someone else.*/
+// import - when you need your code to do something that might have been implemented by someone else
+import (
+	// fmt - contains functions for formatting text
+	// rsc.io/quote - this package collects pithy sayings
+	"fmt"
+	"rsc.io/quote"
+	)
 
-/* fmt
-contains functions for formatting text : Println */
-import "fmt"
-
-/* rsc.io/quote
-this package collects pithy sayings: package quote*/
-import "rsc.io/quote"
-
-/* func main
-create a enter point for go
-*/
+// func - define a function
+// main - create a enter point for go
 func main() {
+	//. Println - used for print and format ouput
+	// quote - this is a package name of the collection of sofware file that come on rsc.io/quote
+	// .Go - this is a function writed inside of quote package
 	fmt.Println(quote.Go())
 }
