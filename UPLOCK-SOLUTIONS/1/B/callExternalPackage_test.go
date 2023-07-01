c136375fa74db82f7054ea34f1ad9d11307caa42
gaBriel nesTor bruFau <70489784+qukiiignb@users.noreply.github.com>
2023-07-01 08:05:46 -0300

    Update callExternalPackage_test.go

--- UPLOCK-SOLUTIONS/1/B/callExternalPackage_test.go
+++ UPLOCK-SOLUTIONS/1/B/callExternalPackage_test.go
@@ -26,6 +26,8 @@ func TestCep(t *testing.T) {
 	// Redirect standard output to a buffer
 	// bytes.Buffer -  is a useful type in Go for working with byte data in memory, providing convenient methods for reading, writing, and manipulating byte slices.
 	var buf bytes.Buffer
+	// os
+	// Stdout
 	old := os.Stdout
 	os.Stdout = &buf
 
