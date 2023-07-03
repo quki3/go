9a4e354e025dc7db7719999ae4d2d77346a552e5
gaBriel nesTor bruFau <70489784+qukiiignb@users.noreply.github.com>
2023-07-03 08:24:50 -0300

    Update Abs.go

--- UPLOCK-SOLUTIONS/1/d/Abs.go
+++ UPLOCK-SOLUTIONS/1/d/Abs.go
@@ -7,11 +7,12 @@ import (
 )
 
 // abs - Abs returns the absolute value of x.
-func abs() {
+func abs(req) {
 	// math - slg
-	x := math.Abs(-2)
-	fmt.Printf("%.1f\n", x)
+	res := math.Abs(req)
+	fmt.Printf("%.1f\n", res)
 
 	y := math.Abs(2)
 	fmt.Printf("%.1f\n", y)
+	return res
 }
