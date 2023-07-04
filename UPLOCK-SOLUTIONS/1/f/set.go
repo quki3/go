8a984fa3f34857c3cd1781c9771971a987a46f02
Quki <70489784+quki3@users.noreply.github.com>
2023-07-04 19:19:48 -0300

    Update set.go

--- UPLOCK-SOLUTIONS/1/f/set.go
+++ UPLOCK-SOLUTIONS/1/f/set.go
@@ -7,12 +7,21 @@ type Person struct {
     name string
     age  int
 }
+// define a struct dog
+type Dog struct {
+    name string
+    age int
+}
 
 // Define a method associated with the Person struct
 func (p Person) SayHello() {
     fmt.Printf("Hello, my name is %s and I am %d years old.\n", p.name, p.age)
     return p.name
 }
+func (d Dog) SayGuau(){
+    fmt.Printf("guau, guau")
+    return d.name
+    }
 
 func main() {
     // Create an instance of the Person object
@@ -20,7 +29,12 @@ func main() {
         name: "John",
         age:  30,
     }
+    dog := Dog{
+        name:"till",
+        age:2,
+        }
 
     // Call the SayHello method on the person object
     person.SayHello()
+    dog.SayGuau()
 }
