2848f3ab4ac00c32cd69880275f8ec71a5d10cbe
Quki <70489784+quki3@users.noreply.github.com>
2023-07-09 09:14:39 -0300

    Update j.go

--- UPLOCK-SOLUTIONS/1/j/j.go
+++ UPLOCK-SOLUTIONS/1/j/j.go
@@ -1,4 +1,7 @@
 package main
+// bufio - provides a convenient set of functions and types for reading and writing data efficiently.
+// fmt - provides basic I/O functionality, including formatted input and output.
+// os - provides a platform-independent interface to operating system functionality. 
 import (
 	"bufio"
 	"fmt"
@@ -6,19 +9,23 @@ import (
 )
 
 func main() {
+            // make - function is a built-in function used to create and initialize certain types of data structures, such as slices, maps, and channels. 
     counts := make(map[string]int)
+	    // bufio.NewScanner - This object can be used to read input from a specified source
+	                      // os.Stdin - refers to the standard input stream, which is the default input source for a Go program.
     input := bufio.NewScanner(os.Stdin)
 
-    // Read input lines until there are no more
+    //***$>_
+    fmt.Println("Enter lines of text (Ctrl+D or Ctrl+Z to stop):")
     for input.Scan() {
         counts[input.Text()]++
     }
-
+    
+    //***$>_
+    fmt.Println("\nCount of each line:")
     // Print lines that appear more than once
     for line, n := range counts {
-        if n > 0 {
             fmt.Printf("%d\t%s\n", n, line)
-        }
     }
 }
 
