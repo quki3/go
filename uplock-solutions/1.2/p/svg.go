22edc37df79a503e4dd256d3f6df36f887772a93
Quki <70489784+quki3@users.noreply.github.com>
2023-07-25 08:43:03 -0300

    Update svg.go

--- uplock-solutions/1.2/p/svg.go
+++ uplock-solutions/1.2/p/svg.go
@@ -15,3 +15,19 @@ constn (
 var sin30, cos30 = math.Sin(angle), math.Cos(angle)
 
 func main(){
+	fmt.Printf("<svg xmlns='http://www.w3.org/2000/svg' "+
+		   "style='stroke: grey; fill: white; stroke-width: 0.7' "+
+		   "width='%d' height='%d'>", width, height)
+	for i := 0; i < cells; i++ {
+		for j := 0; j < cells; j++ {
+			ax, ay := corner(i+1,j)
+			bx, by := corner(i, j)
+			cx, cy := corner(i, j+1)
+			dx, dy := corner(i+1,j+1)
+			fmt.Printf("<polygon points='%g,%g %g,%g %g,%g %g,%g %g' />\n",
+				   ax, ay, by, cx, cy, dx, dy)
+		}
+	}
+	fmt.Println("</svg")
+}
+	
