8b250aa0c39012c392a2c677c6b082859b14a618
Quki <70489784+quki3@users.noreply.github.com>
2023-07-25 09:35:22 -0300

    Update svg.go

--- uplock-solutions/1.2/p/svg.go
+++ uplock-solutions/1.2/p/svg.go
@@ -30,4 +30,20 @@ func main(){
 	}
 	fmt.Println("</svg")
 }
-	
+func corner(i, j int)(float64, float64){
+	//Fint point (x,y) at corner of cell (i,j)
+	x := xyrange * (float64(i)/cells - 0.5)
+	y := xyrange * (float64(j)/cells - 0.5)
+
+	// compute surface height z.
+	z := f(x, y)
+
+	// Project (x,y,z) isometrically onto 2-D SVG canvas (sx, sy).
+	sx := width/2 + (x-y)*cos30*xyscale
+	sy := height/2 + (x+y)*sin30*xyscale - z*zscale
+	return sx, sy
+}
+func f(x, y float64) float64{
+	r := math.Hypot(x,y) //distance from (0,0)
+	return math.Sin(r) /r
+}
