124037754e3c4245b8bff6539ffa091ab7e09ef5
Quki <70489784+quki3@users.noreply.github.com>
2023-07-25 16:07:21 -0300

    Update reports.go

--- uplock-solutions/3.4/b/reports.go
+++ uplock-solutions/3.4/b/reports.go
@@ -2,11 +2,7 @@ package main
 import "fmt"
 
 func main(){
-  fmt.Println(btoi(true))
-  func btoi(b bool) int {
-    if b {
-      return 1
-    }
-    return 0
-  }
+  fmt.Println(itob(1))
+  func itob(i int) bool {return i != 0}
+  
 }
