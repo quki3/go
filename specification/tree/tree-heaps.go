92c3e9f8f2232fd667323afc175671f2bcd5dc7e
quki3 <gabrielbrufau@gmail.com>
2023-10-02 20:31:25 -0300

    02-10-23

---
+++ specification/tree/tree-heaps.go
@@ -0,0 +1,31 @@
+//doc https://www.youtube.com/watch?v=3DYIgTC4T1o&list=PL0q7mDmXPZm7s7weikYLpNZBKk5dCoWm6
+
+package main
+import (
+	"fmt"
+)
+
+	func main() {
+	//[obj] holds
+		type MaxHeap struct{
+			array []int
+		}
+	//[sub]
+	//	insert
+		func (h *MaxHeap) Insert(key int){
+			h.array = append(h.array,key) //insert the value to the array?
+			h.maxHeapifyUp(len(h.array)-1) //this supplies this func with the last index?
+
+		}
+	//	maxHeapifyUp
+		func (h *MaxHeap) maxHeapifyUp(index int){
+			
+		}
+	//	parent
+		func parent(i int) int{
+			return (i-1)/2 //search the position of the parent node
+			//      		1[0]
+			//		10[1]			20[2] <- position is (parent index) * 2 +2
+			//	5[3] <- position is (parent index) * 2 +1
+	//Extract
+}
